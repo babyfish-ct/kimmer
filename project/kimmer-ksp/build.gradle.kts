8774620fc0048b4fa7beb89a6c9b18552035fed0
babyfish-ct <babyfish.ct@gmail.com>
2022-01-16 05:03:35 +0800

    Add publishing configuration

--- project/kimmer-ksp/build.gradle.kts
+++ project/kimmer-ksp/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("org.jetbrains.dokka") version "1.6.10"
+    id("maven-publish")
+    id("signing")
 }
 
 dependencies {
@@ -21,4 +23,58 @@ tasks {
             from("build/dokka/html")
         }
     }
+}
+
+// Publish to maven-----------------------------------------------------
+val NEXUS_USERNAME: String by project
+val NEXUS_PASSWORD: String by project
+
+publishing {
+    repositories {
+        maven {
+            credentials {
+                username = NEXUS_USERNAME
+                password = NEXUS_PASSWORD
+            }
+            name = "MavenCentral"
+            url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            artifactId = "kimmer-ksp"
+            from(components["java"])
+            pom {
+                name.set("kimmer")
+                description.set("KSP for kimmer")
+                url.set("https://github.com/babyfish-ct/kimmer")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/babyfish-ct/kimmer/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("babyfish-ct")
+                        name.set("陈涛")
+                        email.set("babyfish.ct@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/babyfish-ct/kimmer.git")
+                    developerConnection.set("scm:git:ssh://github.com/babyfish-ct/kimmer.git")
+                    url.set("https://github.com//babyfish-ct/kimmer")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
--- project/kimmer/build.gradle.kts
+++ project/kimmer/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.google.devtools.ksp") version "1.6.10-1.0.2"
     id("org.jetbrains.dokka") version "1.6.10"
     `java-library`
+    id("maven-publish")
+    id("signing")
 }
 
 dependencies {
@@ -17,7 +19,7 @@ dependencies {
 
     testImplementation(kotlin("test"))
 
-    kspTest(project(":kimmer-ksp"))
+    kspTest("org.babyfish.kimmer:kimmer-ksp:0.0.0")
 
     dokkaHtmlPlugin("org.jetbrains.dokka:dokka-base:1.6.0")
 }
@@ -42,5 +44,56 @@ tasks {
     }
 }
 
+// Publish to maven-----------------------------------------------------
+val NEXUS_USERNAME: String by project
+val NEXUS_PASSWORD: String by project
 
+publishing {
+    repositories {
+        maven {
+            credentials {
+                username = NEXUS_USERNAME
+                password = NEXUS_PASSWORD
+            }
+            name = "MavenCentral"
+            url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            artifactId = "kimmer"
+            from(components["java"])
+            pom {
+                name.set("kimmer")
+                description.set("Kimmer for kotlin/jvm")
+                url.set("https://github.com/babyfish-ct/kimmer")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/babyfish-ct/kimmer/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("babyfish-ct")
+                        name.set("陈涛")
+                        email.set("babyfish.ct@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/babyfish-ct/kimmer.git")
+                    developerConnection.set("scm:git:ssh://github.com/babyfish-ct/kimmer.git")
+                    url.set("https://github.com//babyfish-ct/kimmer")
+                }
+            }
+        }
+    }
+}
 
+signing {
+    sign(publishing.publications["mavenJava"])
+}
